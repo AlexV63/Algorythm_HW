e57b5b8473f495e44d471696711a519b5ebd4cff
Alexey Vershinin <Alexv4463@gmail.com>
2023-01-29 12:38:54 +0100

    HW 1201 for transfer

--- src/main/java/algo_12_01_2023/HW_12012023.java
+++ src/main/java/algo_12_01_2023/HW_12012023.java
@@ -25,10 +25,10 @@ public class HW_12012023 {
     }
 
     public static void main(String[] args) {
-        int n = 8;
+        int n = 5;
         System.out.println("Число всевозможных «маршрутов» мячика с вершины лесенки " + n
                 + " на землю " + jumpingWays(n));
-    }
+
 
 //    Самый дешёвый путь
 //    В каждой клетке прямоугольной таблицы N*M записано некоторое число.
@@ -37,34 +37,52 @@ public class HW_12012023 {
 //    При проходе через клетку игрок платит определенную сумму, какое число записано в этой клетке.
 //    Требуется найти минимальную стоимость пути, отдав которую игрок может попасть в правый нижний угол.
 
+        int[][] costs = new int[][]{
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0},
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0},
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0},
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0},
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0},
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0},
+                {9, 2, 4, 1, 6, 0, 1, 6, 0, 1, 6, 0}
+        };
 
+        System.out.println(minimalCost(costs, 0, 0));
+        System.out.println(minimalCostMemo(costs));
 
+    }
+        private static int minimalCost ( int[][] costs, int i, int j){
+            if (i >= costs.length || j >= costs[0].length) return 1000000;
+            if (i == costs.length - 1 && j == costs[0].length - 1) return costs[i][j];
+            int minCost = costs[i][j] + Math.min(minimalCost(costs, i + 1, j), minimalCost(costs, i, j + 1));
+            return minCost;
+        }
 
+        private static int minimalCostMemo ( int[][] costs){
+            // cache array init
+            int[][] minCache = new int[costs.length][costs[0].length];
+            for (int i = 0; i < costs.length; i++) {
+                for (int j = 0; j < costs[0].length; j++) {
+                    minCache[i][j] = -1;
+                }
+            }
+            return minCostMemo(costs, minCache, 0, 0);
+        }
 
+        private static int minCostMemo ( int[][] costs, int[][] minCache, int i, int j){
+            // some big number to avoid going beyond the boundaries of the array
+            if (i >= costs.length || j >= costs[0].length) return 100000;
+            // check if the min cost value present in cache
+            if (minCache[i][j] != -1) return minCache[i][j];
+            // lower right corner case
+            if ((i == costs.length - 1) && (j == costs[0].length - 1)) {
+                minCache[i][j] = costs[i][j];
+            } else // recursion formula fills the minCache array
+                minCache[i][j] = costs[i][j] + Math.min(minCostMemo(costs, minCache, i + 1, j), minCostMemo(costs, minCache,
+                        i, j + 1));
+            return minCache[i][j];
+        }
 
-
-//    private static int payMoney() {
-//        int n, m;
-//        Array[][] table = new Array[n][m];
-//
-//        int[][] a;
-//        for (int i = 0; i < n; i++)
-//            for (int j = 0; j < m; j++)
-//                table > a[i][j];
-//
-//        for (int i = 1; i < n; i++) {
-//            a[i][0] += a[i - 1][0];
-//            a[0][i] += a[0][i];
-//        }
-//        for (int i = 1; i < n; i++) {
-//            for (int j = 1; j < m; j++) {
-//                int a1 = a[i - 1][j];
-//                int a2 = a[i][j - 1];
-//                a[i][j] += std::min(a1, a2);
-//            }
-//        }
-//         << a[n - 1][m - 1];
-//    }
 }
 
 
---
+++ src/main/java/algo_19_01_2023/HW190123.java
@@ -0,0 +1,74 @@
+package algo_19_01_2023;
+
+import java.util.Stack;
+
+public class HW190123 {
+//    First level: 1. Реализовать метод search() у написанного на занятии стека MyStack (см. код в репозитории).
+//    public int search(int element) -  определяет, существует ли объект в стеке.
+//    Если элемент найден, возвращает позицию элемента с вершины стека. В противном случае он возвращает -1.
+
+        private int[] array;
+        private int count;
+        public HW190123(int size) {
+            array = new int[size];
+            count = 0;
+        }
+        public void push(int element) {
+            if (count == array.length) throw new RuntimeException("Stack is full");
+            array[count++] = element;
+        }
+
+        public int search(int element) {  //todo  done
+            for (int i = 0; i < array.length; i++) {
+                if (array[i]==element){
+                    return i;
+                }
+             }
+            return -1;
+        }
+
+        public static void main(String[] args) {
+
+            HW190123 myStack = new HW190123(5);
+            myStack.push(1);
+            myStack.push(2);
+            myStack.push(3);
+            myStack.push(4);
+            myStack.push(5);
+
+            System.out.println(myStack.search(3));
+            System.out.println(myStack.search(6));
+
+       }
+
+//    Классическая Queue - реализовать используя массив (Array)
+//    pushToEnd(int data) - вставляет элемент в конец queue
+//    remove() - удаляет элемент из начала queue
+//    peek() - возвращает элемент из начала не удаляя его
+//    isEmpty()
+//    size()
+//    начало:
+
+    public class Queue {
+    private int[] arr;      // массив для хранения элементов queue
+    private int head;      // head указывает на передний элемент в queue
+    private int tail;       // tail часть указывает на последний элемент в queue
+    private int capacity;   // максимальная емкость queue
+    private int count;      // текущий размер queue
+
+     //Конструктор для инициализации queue
+    public Queue(int size)
+    {
+        arr = new int[size];
+        capacity = size;
+        head = 0;
+        tail = -1;
+        count = 0;
+    }
+
+
+
+    // ?? ….. дальше самостоятельно
+
+}
+}
